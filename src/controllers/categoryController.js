2a35f60f6d9b37c5f882313a9886db83a9eec806
bo <diepcaonhan2009@gmail.com>
2025-01-15 14:06:22 +0700

    create category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -3,12 +3,32 @@ export async function listCategory(req, res){
     try {
         const categories = await CategoryModel.find()
         res.render("pages/categories/list",{
-            title:"categories",
+            title:"Categories",
             categories: categories
         })
     } catch (error) {
         console.log(error);
         res.send("hien tai khong co san pham nao!")
+    }
+
+}
+
+export async function renderPageCreateCategory(req, res){
+    res.render("pages/categories/create",{
+        title:"Create Categories",
+    })
+}
+
+export async function createCategory(req, res){
+    const { code , name , image } = req.body
+    try {
+        await CategoryModel.create({
+            code, name, image, createAT: new Date()
+        })
+        res.redirect("/categories")
+    } catch (error) {
+        console.log(error);
+        res.send("tạo sản phẩm không thành công!")
     }
 
 }
--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -5,6 +5,7 @@ const categorySchema = new Schema({
     code: String,
     name: String,
     image: String,
+    createAT: Date
 },{
     versionKey: false,
     collection: "categories"
